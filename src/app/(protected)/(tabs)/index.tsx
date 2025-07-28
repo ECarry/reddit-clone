4290ec69f5419e998fc164c001c923806c3a4e2b
ecarry <lianshiliang93@gmail.com>
2025-07-28 22:18:58 +0800

    feat: fetch posts from Supabase and display them in home feed

--- src/app/(protected)/(tabs)/index.tsx
+++ src/app/(protected)/(tabs)/index.tsx
@@ -1,8 +1,28 @@
 import { FlatList, View } from "react-native";
-import posts from "../../../../assets/data/posts.json";
 import PostListItem from "../../../components/post-list-item";
+import { supabase } from "../../../lib/supabase";
+import { useEffect, useState } from "react";
+import { Post } from "../../../types";
 
 export default function HomeScreen() {
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const fetchPosts = async () => {
+    const { data, error } = await supabase
+      .from("posts")
+      .select("*, group:groups(*), user:users!posts_user_id_fkey(*)");
+
+    if (error) {
+      console.error(error);
+    } else {
+      setPosts(data);
+    }
+  };
+
   return (
     <View>
       <FlatList
